abbb4da0f564ffe729e12fb799865d7f9a1a45b1
Ariel Rezin <arielrezin@gmail.com>
2022-12-29 17:08:02 -0600

    add cask for term app

---
+++ Casks/open-terminal-here.rb
@@ -0,0 +1,11 @@
+cask "open-terminal-here" do
+  version "1.0"
+  sha256 "384b555caa386c0da48839592aebd5723fa801d31f2a4ff4810dbb9e65e3d3a2"
+
+  url "https://github.com/arielrezinn/machine-config/blob/main/finder-toolbar-applications/open-terminal-here/open-terminal-here.dmg?raw=true"
+  name "Open Terminal Here"
+  desc "Add the app to your finder toolbar by holding CMD and dragging it in."
+  homepage "https://github.com/arielrezinn/machine-config/blob/aff2cf5700f25e99763d9a281d605103dbdecd5a/finder-toolbar-applications/open-terminal-here/README.md"
+
+  app "Open Terminal Here.app"
+end
