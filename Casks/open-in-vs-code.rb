c9017f7189d34184d77adc13d0a108248c843a84
Ariel Rezin <arielrezin@gmail.com>
2022-12-29 17:02:36 -0600

    add hash

--- Casks/open-in-vs-code.rb
+++ Casks/open-in-vs-code.rb
@@ -1,7 +1,6 @@
 cask "open-in-vs-code" do
   version "1.0"
-  # sha256 "31390b7e71fc8e75ab12f2279c8e75d09babab24feb88b93e77a13ae121a32b5"
-  sha256 ":no_check"
+  sha256 "31390b7e71fc8e75ab12f2279c8e75d09babab24feb88b93e77a13ae121a32b5"
 
   url "https://github.com/arielrezinn/machine-config/blob/12ba19f3644f8ea72a64d316614f68690b628b3e/finder-toolbar-applications/open-in-vscode/open-in-vs-code.dmg?raw=true"
   name "Open in VS Code"
